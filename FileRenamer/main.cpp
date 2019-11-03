07104eb1180e3885b2a1d2d4d646fa014c558f26
Tresvian <44061648+Tresvian@users.noreply.github.com>
2019-11-02 23:58:51 -0500

    fixed logic on start

--- FileRenamer/main.cpp
+++ FileRenamer/main.cpp
@@ -13,13 +13,13 @@ Error Codes
 
 int main(int argc, char* argv[])
 {
-	if (argc > 3 && argc < 2)
+	if (argc > 3 || argc < 2)
 	{
 		std::cerr << "Invalid argument count, use /?" << std::endl;
 		return -1;
 	}
 
-	if (std::string(argv[2]) == std::string("/?"))
+	if (std::string(argv[1]) == std::string("/?"))
 	{
 		std::cout << "Help dialogue enabled" << std::endl;
 		std::cout << "Usage:" << std::endl;
@@ -29,19 +29,29 @@ int main(int argc, char* argv[])
 		return 1;
 	}
 
-	// Sanitize, formed to std::string at size
-	if (std::string(argv[1]).size() > std::string(argv[1]).max_size() && std::string(argv[2]).size() > std::string(argv[2]).max_size())
-	{
-		std::cerr << "Input too large" << std::endl;
-		return -2;
-	}
-
 	int programExit;
 
+
 	if (argc == 2)
+	{
+		// Sanitize, formed to std::string at size
+		if (std::string(argv[1]).size() > std::string(argv[1]).max_size())
+		{
+			std::cerr << "Input too large" << std::endl;
+			return -2;
+		}
 		programExit = mainRoutine(std::string(argv[1]), std::string(argv[2]));
+	}
 	else
+	{
+		// Sanitize, formed to std::string at size
+		if (std::string(argv[1]).size() > std::string(argv[1]).max_size() || std::string(argv[2]).size() > std::string(argv[2]).max_size())
+		{
+			std::cerr << "Input too large" << std::endl;
+			return -2;
+		}
 		programExit = mainRoutine(std::string(argv[1]), std::string("DEPENDENT=FALSE"));
+	}
 
 	return programExit;
 }
